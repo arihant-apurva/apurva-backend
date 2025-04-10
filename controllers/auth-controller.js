ef205f11b13c5b8b25e57cba6a01a0649e2d477c
arihant-apurva <arihant.apurvasoftwaresolutions@gmail.com>
2025-04-10 17:10:14 +0530

    samesite cookie changes

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -67,7 +67,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "Lax",
+            sameSite: "None",
             maxAge: 60 * 60 * 1000 // 1 hour
         });
 
