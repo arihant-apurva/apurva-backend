c987d600c2c61d3eb3c473c85f59f97489a4b641
arihant-apurva <arihant.apurvasoftwaresolutions@gmail.com>
2025-04-10 12:57:20 +0530

    samesite become Lax

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -67,7 +67,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "None",
+            sameSite: "Lax",
             maxAge: 60 * 60 * 1000 // 1 hour
         });
 
